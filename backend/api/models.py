ece6cbf8be6d955b1be0301eac93c6204215af2b
Edely <edely.gomes@convergenceworks.com.br>
2019-02-21 18:44:39 -0300

    starts to add models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    nome = models.CharField(max_length=20)
+    descricao = models.TextField()
+    responsavel = models.CharField(max_length=20)
+    prazo = models.DateTimeField()
+    created = models.DateTimeFiel(auto_now_add=True)

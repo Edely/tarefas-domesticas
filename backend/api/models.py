3caa1ff973b2578c269e6a45795ff7a50f37418f
Edely <edely.gomes@convergenceworks.com.br>
2019-02-26 08:33:55 -0300

    Change Task Model to Use User as FK

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.conf import settings
 
 
 class Task(models.Model):
     nome = models.CharField(max_length=20)
     descricao = models.TextField()
-    responsavel = models.CharField(max_length=20)
+    responsavel = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
     prazo = models.DateTimeField()
     created = models.DateTimeField(auto_now_add=True)
     feita = models.BooleanField(default=False)

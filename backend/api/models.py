327ba148f8443a52b37b846e87e08a273cd84564
Edely <edely.gomes@convergenceworks.com.br>
2019-03-07 09:54:41 -0300

    Change words from pt to en

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,12 +3,12 @@ from django.conf import settings
 
 
 class Task(models.Model):
-    nome = models.CharField(max_length=20)
-    descricao = models.TextField()
-    responsavel = models.ForeignKey(
+    name = models.CharField(max_length=20)
+    description = models.TextField()
+    owner = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
-    prazo = models.DateTimeField()
+    deadline = models.DateTimeField()
     created = models.DateTimeField(auto_now_add=True)
-    feita = models.BooleanField(default=False)
+    done = models.BooleanField(default=False)
